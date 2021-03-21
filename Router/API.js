99e54f35f3a4bb04ec076a2e6f353771882854c7
Jesse Huang <jesse.huang1205@gmail.com>
2021-03-21 19:57:07 -0400

    Add files via upload

--- Router/API.js
+++ Router/API.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const bodyparser = require('body-parser');
 const router = express.Router();
 const db = require('./database');
 const opnv = require('../Models/nvop');
@@ -21,7 +22,7 @@ router.get('/getOP', async (req, res) => {
     // opnv.findOne({order:sequelize.random()})
     //     .then(encounter => {res.status(200).json({opening:encounter.name,move:encounter.moves})})
     //     .catch(err => console.log(err));
-    let rand = Math.floor(Math.random()*3);
+    let rand = Math.floor(Math.random()*4);
     console.log(rand);
     await opnv.findOne({order:sequelize.random()})
         .then(async d =>{
@@ -31,15 +32,17 @@ router.get('/getOP', async (req, res) => {
                 res.sendStatus(404);
             switch(rand){
                 case 0:
-                    res.status(200).json({question:`Given the ${d.name}. What is the Correct move order`,answer:d.moves,ww:stats.white,bw:stats.black,dr:stats.draws});
+                    res.status(200).json({question:`Given the ${d.name}. What is the Correct move order`,movesplayed:"",answer:d.moves,ww:stats.white,bw:stats.black,dr:stats.draws});
                     break;
                 case 1:
                     let move = d.moves.split(' ');
-                    res.status(200).json({question:`Given that they are playing ${d.name}. What is the correct next move?`,answer:move[move.length-1],ww:stats.white,bw:stats.black,dr:stats.draws});
+                    res.status(200).json({question:`Given that they are playing ${d.name}. What is the correct next move?`,movesplayed:d.moves.substring(0,d.moves.length-4),answer:move[move.length-1],ww:stats.white,bw:stats.black,dr:stats.draws});
                     break;
                 case 2:
-                    res.status(200).json({question:`You want to play the ${d.name}. What is the first move?`,answer:d.moves.split(' ')[1],ww:stats.white,bw:stats.black,dr:stats.draws});
+                    res.status(200).json({question:`You want to play the ${d.name}. What is the first move?`,movesplayed:"",answer:d.moves.split(' ')[0],ww:stats.white,bw:stats.black,dr:stats.draws});
                     break;
+                case 3:
+                    res.status(200).json({question:`What is this opening?`,moveplayed:d.moves,answer:d.name,ww:stats.white,bw:stats.black,dr:stats.draws});
             }
             }
         )
@@ -48,16 +51,19 @@ router.get('/getOP', async (req, res) => {
 
 });
 
-router.get('/stats',(req,res) => {
+router.get('/stats/',(req,res) => {
 
-    fetch('https://explorer.lichess.ovh/lichess?variant=standard&speeds[]=blitz&speeds[]=rapid&speeds[]=classical&ratings[]=2200&ratings[]=2500&fen=rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq')
+    let fen = req.body.fen;
+    console.log(fen);
+    fetch(`https://explorer.lichess.ovh/lichess?variant=standard&speeds[]=blitz&speeds[]=rapid&speeds[]=classical&ratings[]=2200&ratings[]=2500&fen=${fen}`)
         .then(res => res.json())
         .then(txt => {
             
-            console.log(txt);
+            console.log("Got data");
             res.status(200).send(txt);
 
-        });
+        })
+        .catch(err => res.sendStatus(400));
 
 });
 
